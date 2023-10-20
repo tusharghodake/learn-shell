c4fa1a7914be37df226fc3bac44da17e7e6192a7
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-20 23:07:54 +0530

    03-variable script added.

---
+++ 03-variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#!/usr/bin/env bash
+
+#This is variable example
+
+name=Tushar
+sirname = G
+
+echo 'My Name is' $Name $sirname
+
+#DATE-2023-10-20
+DATE = $(date +%F)
+echo 'Todays date is' $DATE
