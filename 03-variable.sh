dd56a187a03fb87799bb60bd5e7a688640f93198
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-20 23:19:25 +0530

    03-variable script updated.

--- 03-variable.sh
+++ 03-variable.sh
@@ -7,9 +7,12 @@ name=Tushar
 class=Shell-Script
 
 echo My Name is $name
-echo My Class Name is $class
+echo My Class Name is ${class}
 
 #DATE-2023-10-20
 DATE=$(date +%F)
 
 echo Todays date is $DATE
+
+ADD=$((2+5))
+echo addition is $ADD
