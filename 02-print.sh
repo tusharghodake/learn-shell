c8b6d0ede95a1f36f75307c54db496f3182c22a4
Tushar Ghodke <tushar.d.ghodake@gmail.com>
2023-10-22 14:42:24 +0530

    code updated

--- 02-print.sh
+++ 02-print.sh
@@ -21,3 +21,8 @@ echo -e "\e[34mMESSAGE\e[0m"
 echo -e "\e[35mMESSAGE\e[0m"
 echo -e "\e[36mMESSAGE\e[0m"
 
+# https://misc.flogisoft.com/bash/tip_colors_and_formatting
+
+echo -e "Line1\nLine2"
+
+echo -e "Line1\t\tLine2"
